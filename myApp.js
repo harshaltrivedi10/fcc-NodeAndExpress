8fbe918264c8b90e3573c944a912f0d345cdde27
Harshal Trivedi <harshal30897@gmail.com>
2022-06-25 10:56:05 -0700

    Handled default route - challenge 2.

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let app = express();
 
+// Challenge 1 - output something to the console
+console.log("Hello World!");
 
-
+app.get("/", function (req, res) {
+  res.send("Hello Express");
+});
 
 
 
